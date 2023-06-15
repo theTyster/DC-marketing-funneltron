cc949a666525df2de50d7cd8978ac4f7ee6247e7
Ty <noreply@theducksays.click>
2023-06-14 18:19:43 -0700

    no commit message

--- code/auth.py
+++ code/auth.py
@@ -6,7 +6,7 @@ feed_auth = 'https://spreadsheets.google.com/feeds'
 spreadsheets_auth = 'https://www.googleapis.com/auth/spreadsheets'
 file_auth = 'https://www.googleapis.com/auth/drive.file' 
 drive_auth = 'https://www.googleapis.com/auth/drive'  
-json_loc = '/Users/tydavis/Documents/code/Work/Python/gsheet-cred.json'
+json_loc = 'gsheet-cred.json'
 
 scope = [feed_auth, spreadsheets_auth, file_auth, drive_auth]
 creds = ServiceAccountCredentials.from_json_keyfile_name(json_loc, scope)
